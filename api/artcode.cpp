e46fe93d1428783dcb55c5b181ce00eff4cb9df9
Jummer Ramos <jumjumramos@gmail.com>
2026-04-20 04:29:42 +0800

    added sources implementation config init and wip

--- api/artcode.cpp
+++ api/artcode.cpp
@@ -1,1 +1,23 @@
 #include "artcode.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+// Build system
+// TODO: create source struct implementation
+struct Sources::Source {
+    std::vector<std::string> includes;
+};
+
+Sources::Sources() : _sources(std::make_unique<Source>()) {};
+
+Sources::~Sources() = default;
+
+void Sources::add() {
+
+};
+
+void Sources::build() {
+
+};
--- api/artcode.hpp
+++ api/artcode.hpp
@@ -2,9 +2,10 @@
 
 #include <glm/fwd.hpp>
 
+#include <memory>
+
 // Forward declarations
 // build system
-struct ISources;
 struct Sources;
 
 // artcode
@@ -15,7 +16,23 @@ struct Square;
 struct Circle;
 
 // Vectors data types
-typedef glm::vec1 AcV1;
-typedef glm::vec2 AcV2;
-typedef glm::vec3 AcV3;
-typedef glm::vec4 AcV4;
+typedef glm::vec1 Vec1;
+typedef glm::vec2 Vec2;
+typedef glm::vec3 Vec3;
+typedef glm::vec4 Vec4;
+
+// sources
+struct Sources {
+  public:
+    Sources();
+
+    ~Sources();
+
+    void add();
+
+    void build();
+
+  private:
+    struct Source;
+    std::unique_ptr<Source> _sources;
+};
