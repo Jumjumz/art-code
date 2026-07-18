f7d75d9c1a4be24e27037931d3b3a3d812b18db1
Junner Ramos <jumjumramos@gmail.com>
2026-07-19 04:38:44 +0800

    added skewed rectangle in test

--- test/main.cpp
+++ test/main.cpp
@@ -7,12 +7,22 @@ int main() {
 
     std::cout << comp.x << std::endl;
 
-    // quad example specifically square
+    // quad example i.e square
     Art::Quad qd;
     qd.w        = 100;
     qd.l        = 100;
     qd.position = {300, 300};
     qd.color    = "#322322";
+
+    // skewed quad example i.e rectangle
+    Art::Quad rc;
+    rc.w        = 300;
+    rc.l        = 100;
+    rc.fill     = true;
+    rc.position = {400, 300};
+    rc.skew     = true;
+    rc.skewPos  = {{{Vec2{100, 200}, 2}, {Vec2{-200, 400}, 4}}};
+    rc.color    = "#696969";
 
     Art::Draw();
 
